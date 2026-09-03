e4d898f647f1232cd626e056c41077898c8dc4eb
renan-prieto <prietodasilvarenan@gmail.com>
2026-09-02 22:42:18 -0300

    feat: function to insert data

--- sudoku.py
+++ sudoku.py
@@ -28,6 +28,7 @@ def mount_board():
 
 def remove_tips(diff, board):
     count = 0
+    copy_board = board
     while True:
         for i in range(9):
             for j in range(9):
@@ -35,13 +36,13 @@ def remove_tips(diff, board):
                     break
                 removed_tip = randint(0, BOARD_SIZE)
                 if removed_tip < diff:
-                    board[i][j] = '_'
+                    copy_board[i][j] = '_'
                     count += 1
         if count >= diff: 
             break
-    print(count)
+    return copy_board
 
-def show_board():
+def show_board(board):
     print("  ", *ALPHABET[0:3]," ", *ALPHABET[3:6:], " ", *ALPHABET[6:9:] )
     for i in range(0, 9):
         print( NUMBERS[i], "", *sudoku_board[i][0:3], "║", *sudoku_board[i][3:6], "║",  *sudoku_board[i][6:9])
@@ -49,7 +50,24 @@ def show_board():
             # ╬ ═
             print(f"   {6*'═'}╬{7*'═'}╬{6*'═'}" )
 
+def append_value(letter, number, value):
+    if copy_board[letter][number] != '_':
+        return f"Nao foi possivel adicionar valores: {letter}, {number} ja preenchidos!"
+    elif value == board[letter][number]:
+        copy_board[letter][number] = value
+        return copy_board
+
+def inserting_data():
+    print("exemplo: A1 2")
+    value_and_cord = input(">")
+    letter = value_and_cord[0:1]
+    number = value_and_cord[1:2]
+    value  = value_and_cord[3:4]
+    return letter, number, value
 
 sudoku_board = mount_board()
-remove_tips(EASY, sudoku_board)
-show_board()
+copy_board = remove_tips(EASY, sudoku_board)
+show_board(copy_board)
+
+
+print(letter, number, value)
